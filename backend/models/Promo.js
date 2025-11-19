8640e008be1e27bf35d2e387015ed178dc3d4694
Nishant Jha <145684364+nishant-jha72@users.noreply.github.com>
2025-11-19 19:19:45 +0530

    Update Promo.js

--- backend/models/Promo.js
+++ backend/models/Promo.js
@@ -1,10 +1,13 @@
+// models/Promo.js
 const mongoose = require('mongoose');
 
 const promoSchema = new mongoose.Schema({
-  title: {type: String, required: true},
-  description: String,
-  promoter: {type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true},
-  interestedClients: [{type: mongoose.Schema.Types.ObjectId, ref: 'User'}]
-}, {timestamps: true});
+  title: { type: String, required: true },
+  description: { type: String },
+  promoter: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true },
+  open: { type: Boolean, default: true },
+  promotedAt: { type: Date, default: Date.now },
+  interestedClients: [{ type: mongoose.Schema.Types.ObjectId, ref: 'User' }]
+});
 
 module.exports = mongoose.model('Promo', promoSchema);
